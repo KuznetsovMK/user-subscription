aa0135124d450afd79800367a618a89de00ddb6d
Кузнецов Михаил Владимирович <Kuznetsov.M@russianpost.ru>
2025-05-06 15:27:33 +0300

    first commit

---
+++ src/main/java/web_rize/user_subscription/UserSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package web_rize.user_subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSubscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSubscriptionApplication.class, args);
+	}
+
+}
