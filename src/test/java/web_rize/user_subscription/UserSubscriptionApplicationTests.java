3019dec3cac55ef1482acc08206f0cd8e26f6237
Кузнецов Михаил Владимирович <Kuznetsov.M@russianpost.ru>
2025-05-06 15:27:33 +0300

    add dependencies

---
+++ src/test/java/web_rize/user_subscription/UserSubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package web_rize.user_subscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSubscriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
